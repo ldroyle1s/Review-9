096386ea35f61792430cf284c07ad4479d366297
LDRoyle1s <LDRoyle1s@capetigers.com>
2026-01-20 10:35:16 -0600

    At Step 5C

--- js/tny_timer.js
+++ js/tny_timer.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
@@ -8,7 +10,13 @@
    Date:   
 
 */
-
+showClock();
+function showClock() {
+   var thisDay = new Date("May 19, 2018 9:31:27");
+   var localDate = thisDay.toLocaleDateString();
+   var localTime = thisDay.toLocaleTimeString();
+   document.getElementById("currentTime").innerHTML ="<span>" + localDate + "</span>" + "<span>" + localTime + "</span>";
+}
 
 function nextJuly4(currentDate) {
    var cYear = currentDate.getFullYear();
